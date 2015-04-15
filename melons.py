9f5163b749076e68e4165d62c4a10c9d6a48b0fc
Hackbright Student <info@hackbrightacademy.com>
2015-04-15 16:32:03 -0700

    Added Melon Superclass

--- melons.py
+++ melons.py
@@ -1,123 +1,117 @@
 """This file should have our melon-type classes in it."""
+class Melon(object):
+    
+    def get_base_price(self):
+        return 5.00
 
-
-class WatermelonOrder(object):
+class WatermelonOrder(Melon):
     species = "Watermelon"
     color = "green"
     imported = False
     shape = 'natural'
     seasons = ['Fall', 'Summer']
-    base_price = 5.0
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
         if qty >= 3:
-            return qty * self.base_price * .75 
+            return qty * Melon.get_base_price() * .75 
         else:
-            return qty * self.base_price
+            return qty * Melon.get_base_price()
 
-class Cantaloupe(object):
+class Cantaloupe(Melon):
     species = "Cantaloupe"
     color = "tan"
     imported = False
     shape = "natural"
     seasons = ['Spring', 'Summer']
-    base_price = 5.0
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
         if qty >=5:
-            return qty * self.base_price * .5
+            return qty * Melon.get_base_price() * .5
         else:
-            return qty * self.base_price
+            return qty * Melon.get_base_price()
 
         
 
-class Casaba(object):
+class Casaba(Melon):
     species = "Casaba"
     color = 'green'
     imported = True
     shape = "natural"
     seasons = ['Spring','Summer', 'Fall', 'Winter']
-    base_price = 9.0
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
-        return qty * self.base_price
+        return qty * ((Melon.get_base_price() + 1) * 1.5)
 
-class Sharlyn(object):
+class Sharlyn(Melon):
     species = "Sharlyn"
     color = "tan"
     imported = True
     shape = "natural"
     seasons = ["Summer"]
-    base_price = 7.50
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
-        return qty * self.base_price
+        return qty * (Melon.get_base_price() * 1.5)
 
-class SantaClaus(object):
+class SantaClaus(Melon):
     species = "Santa Claus"
     color = "green"
     imported = True
     shape = "natural"
     seasons = ['Winter', "Spring"]
-    base_price = 7.50 
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
-        return qty * self.base_price
+        return qty * (Melon.get_base_price() * 1.5)
 
-class Christmas(object):
+class Christmas(Melon):
     species = "Christmas"
     color = "green"
     imported = False
     shape = "natural"
     seasons = ["Winter", "Spring"]
-    base_price = 5.00
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
-        return qty * self.base_price
+        return qty * Melon.get_base_price()
 
-class HornedMelon(object):
+class HornedMelon(Melon):
     species = "Horned Melon"
     color = "yellow"
     imported = True
     shape = "natural"
     seasons = ["Summer"]
-    base_price = 7.50
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
-        return qty * self.base_price
+        return qty * (Melon.get_base_price() * 1.5)
 
-class Xigua(object):
+class Xigua(Melon):
     species = "Xigua"
     color = "black"
     imported = True
     shape = "square"
     seasons = ["Summer"]
-    base_price = 15.0
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
-        return qty * self.base_price
+        return qty * (Melon.get_base_price() * 2 * 1.5)
 
-class Ogen(object):
+class Ogen(Melon):
     species = "Ogen"
     color = "tan"
     imported = False
     shape = "natural"
     seasons = ["Spring", "Summer"]
-    base_price = 6.00 
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
-        return qty * self.base_price
+        return qty * (Melon.get_base_price() + 1)
