32f7b86650da4ae576e6acd351eafbefcd76d007
Hackbright Student <info@hackbrightacademy.com>
2015-04-15 12:46:28 -0700

    Wrote out classes for all melons

--- melons.py
+++ melons.py
@@ -7,6 +7,120 @@ class WatermelonOrder(object):
     imported = False
     shape = 'natural'
     seasons = ['Fall', 'Summer']
+    base_price = 5.0
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class Cantaloupe (object):
+    species = "Cantaloupe"
+    color = "tan"
+    imported = False
+    shape = "natural"
+    seasons = ['Spring', 'Summer']
+    base_price = 5.0
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class Casaba (object):
+    species = "Casaba"
+    color = 'green'
+    imported = True
+    shape = "natural"
+    seasons = ['Spring','Summer', 'Fall', 'Winter']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class Sharlyn (object):
+    species = "Sharlyn"
+    color = "tan"
+    imported = True
+    shape = "natural"
+    seasons = ["Summer"]
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class SantaClaus (object):
+    species = "Santa Claus"
+    color = "green"
+    imported = True
+    shape = "natural"
+    seasons = ['Winter', "Spring"]
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class Christmas(object):
+    species = "Christmas"
+    color = "green"
+    imported = False
+    shape = "natural"
+    seasons = ["Winter", "Spring"]
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class HornedMelon(object):
+    species = "Horned Melon"
+    color = "yellow"
+    imported = True
+    shape = "natural"
+    seasons = ["Summer"]
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class Xigua(object):
+    species = "Xigua"
+    color = "black"
+    imported = True
+    shape = "square"
+    seasons = ["Summer"]
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class Ogen(object):
+    species = "Ogen"
+    color = "tan"
+    imported = False
+    shape = "natural"
+    seasons = ["Spring", "Summer"]
 
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
